396e90a1bdab2f1ae597d7a6af7bc1d44f0e06d3
Aidablaya <you@example.com>
2023-06-10 18:44:18 +0200

    structure

---
+++ src/js/01_list.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const ulElement =document.querySelector('.js_listUl');
+const url = 'https://api.disneyapi.dev/character';
+
+//solicitud al servidor
+fetch(url)
+.then((response)=> response.json())
+.then((data)=>{
+  listCharacterdApi = data.character;
+  renderCard = (listCharacterdApi);
+    console.log(data);
+    ulElement.innerHTML = renderCard();
+});
+
+//función para renderizar carta
+function renderCharacter(character){
+    let html= `<li>
+    <div class="card">
+      <img class="card__img js_imgCard" src="" alt="">
+      <p class="card__text js_textCard"></p>
+    </div>
+  </li>`;
+  return html;
+};
+console.log(renderCharacter());
