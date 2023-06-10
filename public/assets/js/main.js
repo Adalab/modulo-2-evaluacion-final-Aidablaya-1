181e93fc86879349deea095062d247d4c59718ec
Aidablaya <you@example.com>
2023-06-10 20:50:19 +0200

    added list API

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,29 +1,40 @@
 'use strict';
 
+//variables globales
 const ulElement =document.querySelector('.js_listUl');
 const url = 'https://api.disneyapi.dev/character';
+
+let listCharacterApi = [];
 
 //solicitud al servidor
 fetch(url)
 .then((response)=> response.json())
-.then((data)=>{
-  listCharacterdApi = data.character;
-  renderCard = (listCharacterdApi);
+.then((data)=>{  
     console.log(data);
-    ulElement.innerHTML = renderCard();
+    listCharacterApi = data.data; 
+    renderCharacterList(listCharacterApi);
 });
 
-//función para renderizar carta
-function renderCharacter(character){
-    let html= `<li>
-    <div class="card">
-      <img class="card__img js_imgCard" src="" alt="">
-      <p class="card__text js_textCard"></p>
-    </div>
+//función renderizar toda la lista de characters
+function renderCharacterList(listData){
+    for(const character of listData){
+      ulElement.innerHTML += renderCharacter(character);
+      
+    }
+};
+
+//función para renderizar personaje
+function renderCharacter(characters){
+    let html= `<li card">
+      <div class="character">
+        <img class="character__img js_imgCard" src="${characters.imageUrl}" alt="">
+        <p class="character__text js_textCard">${characters.name}</p>
+      </div>
   </li>`;
   return html;
+;
 };
-console.log(renderCharacter());
+
 'use strict';
 
 console.log('>> Ready :)');
