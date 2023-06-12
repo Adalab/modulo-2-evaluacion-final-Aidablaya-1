f0288546087754b8a5108d85a7baed456b1da19d
Aidablaya <you@example.com>
2023-06-12 14:10:27 +0200

    search button just

--- src/js/01_list.js
+++ src/js/01_list.js
@@ -4,15 +4,43 @@
 const ulElement =document.querySelector('.js_listUl');
 const url = 'https://api.disneyapi.dev/character';
 const ulFavorites =document.querySelector('.js_characterFavorite');
+const inputSearch= document.querySelector('.js_search');
+const btnSearch = document.querySelector('.js_btnSearch');
 
 let listCharacterApi = [];
 let nextPageUrl= 'http://api.disneyapi.dev/character?page=2&pageSize=50';
 
 let listCharacterFavorite = [];
 
+//verificar si hay cosas en el localStorage
+const characterLocalStorage = JSON.parse(localStorage.getItem('character'));
+console.log (characterLocalStorage);
+
+function init () {
+  if (characterLocalStorage) {
+    listCharacterApi = characterLocalStorage;
+    renderCharacterList(listCharacterApi);
+  } else {
+    fetch(url)
+      .then((response)=> response.json())
+      .then((data)=> {  
+        console.log(data);
+        listCharacterApi = data.data; 
+
+        console.log (listCharacterApi);
+        renderCharacterList(listCharacterApi);
+        //añadido para poner paginación
+        nextPageUrl= data.next;
+        //coger dato para guardarlos
+        localStorage.setItem('character', JSON.stringify(listCharacterApi));
+})
+  }
+};
+
+init();
 /////////////////////////////////////////////////////////FETCH DISNEY/////////////////////////////
 //solicitud al servidor
-fetch(url)
+/*fetch(url)
 .then((response)=> response.json())
 .then((data)=>{  
     console.log(data);
@@ -21,8 +49,10 @@ fetch(url)
     renderCharacterList(listCharacterApi);
     //añadido para poner paginación
     nextPageUrl= data.next;
-});
-console.log (localNextPage())
+    //coger dato para guardarlos
+    localStorage.setItem('character', JSON.stringify(listCharacterApi));
+});*/
+
 
 //function para mostrar la siguiente pagina
 function localNextPage() {
@@ -30,19 +60,24 @@ function localNextPage() {
     fetch(nextPageUrl)
       .then((response) => response.json())
       .then((data) => {
+        console.log (listCharacterApi);
         listCharacterApi = listCharacterApi.concat(data.data);
+        console.log (listCharacterApi);
         renderCharacterList(listCharacterApi);
         nextPageUrl = data.next;
+        localStorage.setItem('character', JSON.stringify(listCharacterApi));
       })
-      
+      ///ejecutar funcion
   }
 };
+
 
 
 //////////////////////////////////////////////FUNCIONES//////////////////////////////////////////////////////////////////////////////
 
 //función renderizar toda la lista de characters//recorrer array
 function renderCharacterList(listData){
+    ulElement.innerHTML = '';
     for(const character of listData){
       ulElement.innerHTML += renderCharacter(character);      
     }
@@ -72,7 +107,7 @@ function renderCharacter(characters){
   return html;
 };
 
-/////////////////////////LISTA DE FAVORITOS /////////////////////////////////////////////////////////////////
+///////////////////////// LISTA DE FAVORITOS /////////////////////////////////////////////////////////////////
 
 //recojo elemento li// aádir array de favoritos y meter dentro lo que clicamos
 function handleClick (ev) {
@@ -88,7 +123,8 @@ function handleClick (ev) {
   } else {
     listCharacterFavorite.splice(indexCharacter, 0);
   }
-  renderFavoriteList();
+  //guardar la lista en local Storage
+    renderFavoriteList();
 };
 
 // funcion añadidora de personajes a la lista de fav
@@ -101,3 +137,14 @@ function renderFavoriteList (){
 
 /////////////////////////////////BUSCADOR///////////////////////////////////////////////////////////////////////
 
+const handleClickSearch = (ev) => {
+  ev.preventDefault();
+  const inputValue = inputSearch.value;
+  const filterList = listCharacterApi.filter((item)=>
+    item.name.toLowerCase().includes(inputValue.toLowerCase())
+  );
+  console.log(filterList);
+  renderCharacterList(filterList);
+};
+
+btnSearch.addEventListener('click', handleClickSearch);
