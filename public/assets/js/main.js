3c1f27b3c872a8564792de6ef1f54ed474cff7ac
Aidablaya <you@example.com>
2023-06-12 11:54:32 +0200

    finish function favorite

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -3,9 +3,12 @@
 //variables globales
 const ulElement =document.querySelector('.js_listUl');
 const url = 'https://api.disneyapi.dev/character';
+const ulFavorites =document.querySelector('.js_characterFavorite');
 
 let listCharacterApi = [];
 let nextPageUrl= 'http://api.disneyapi.dev/character?page=2&pageSize=50';
+
+let listCharacterFavorite = [];
 
 /////////////////////////////////////////////////////////FETCH DISNEY/////////////////////////////
 //solicitud al servidor
@@ -14,6 +17,7 @@ fetch(url)
 .then((data)=>{  
     console.log(data);
     listCharacterApi = data.data; 
+    console.log (listCharacterApi);
     renderCharacterList(listCharacterApi);
     //añadido para poner paginación
     nextPageUrl= data.next;
@@ -42,20 +46,24 @@ function renderCharacterList(listData){
     for(const character of listData){
       ulElement.innerHTML += renderCharacter(character);      
     }
-    const liElements = document.querySelectorAll('.js_card');
+    addEventId();      
+    }
+;
+
+//recorro elemento li
+function addEventId() {
+  const liElements = document.querySelectorAll('.js_card');  
     //necesito otro bucle para recorrer lis
     for(const li of liElements){
       li.addEventListener('click', handleClick);
-    }
-
-};
+}};
 
 //función para renderizar personaje(foto y nombre)
 function renderCharacter(characters){
   
     let imageUrl = characters.imageUrl || './assets/images/imagenRepuesto.png';
 
-    let html = `<li class = "card js_card" id=${characters.id}>
+    let html = `<li class = "card js_card" id=${characters._id}>
       <div class="character">
         <img class="character__img js_imgCard" src=${characters.imageUrl} alt="">
         <p class="character__text js_textCard">${characters.name}</p>
@@ -64,11 +72,34 @@ function renderCharacter(characters){
   return html;
 };
 
+/////////////////////////LISTA DE FAVORITOS /////////////////////////////////////////////////////////////////
 
+//recojo elemento li// aádir array de favoritos y meter dentro lo que clicamos
 function handleClick (ev) {
-  const id = ev.currentTarget.id;
-  console.log(id);
+  const id = parseInt(ev.currentTarget.id);
+  //buscar dentro de array el primer elemento que cumple con la condicion(devuelve el contenido)
+  const selectedCharacter = listCharacterApi.find((item)=> item._id === id);
+  //devolver posición de array favoritos
+  const indexCharacter = listCharacterFavorite.findIndex((item)=> item._id === id);
+  
+  //validar
+  if( indexCharacter === -1 ){
+    listCharacterFavorite.push(selectedCharacter);
+  } else {
+    listCharacterFavorite.splice(indexCharacter, 0);
+  }
+  renderFavoriteList();
 };
+
+// funcion añadidora de personajes a la lista de fav
+function renderFavoriteList (){
+  ulFavorites.innerHTML='';
+  for(const fav of listCharacterFavorite){
+    ulFavorites.innerHTML += renderCharacter(fav);
+  }
+};
+
+/////////////////////////////////BUSCADOR///////////////////////////////////////////////////////////////////////
 
 
 'use strict';
